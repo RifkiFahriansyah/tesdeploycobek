066d4d4bc8bf84bd48483ab09f9fd4cd53dbcdf3
Rifki Fahriansyah <rifkifahriansyah_2327250093@mhs.mdp.ac.id>
2025-12-11 08:56:49 +0700

    tes allowed origin

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+    'https://cobekorder.vercel.app',
+    'http://localhost:5173'],
 
     'allowed_origins_patterns' => [],
 
